2affdef7f93cd7e786b24184d40ee023cb6b7e12
st9149201 <st9149201@stud.duikt.edu.ua>
2024-12-08 11:47:04 +0200

    +add FireBase Cloud Messaging

--- app/src/main/java/com/olivinskij/smokepro/MainActivity.java
+++ app/src/main/java/com/olivinskij/smokepro/MainActivity.java
@@ -1,20 +1,41 @@
 package com.olivinskij.smokepro;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
 import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+
+    private ActivityResultLauncher<String> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted){
+                    getDeviceToken();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        requestPermission();
 
         LottieAnimationView lottie = findViewById(R.id.lottiesplash);
 
@@ -43,5 +67,33 @@ public class MainActivity extends AppCompatActivity {
         lottie.setRepeatCount(ValueAnimator.INFINITE);
 
 
+    }
+
+    public void requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED){
+            }else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)){
+            }else {
+                resultLauncher.launch(POST_NOTIFICATIONS);
+            }
+        }else {
+            //GEt Device Token from Firebase
+            getDeviceToken();
+        }
+    }
+
+    public void getDeviceToken(){
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()){
+                    Log.e("FireBaseLogs","Fetching Toke Failed"+ task.getException());
+                    return;
+                }
+                String token = task.getResult();
+                Log.v("FireBaseLogs","Device Token: "+token);
+            }
+        });
     }
 }
